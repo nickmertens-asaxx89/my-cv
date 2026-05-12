ad17db20c98c38505b589a442dcfddf19953fde1
Nick Mertens <devmac@MacBook-Pro-van-Nick.local>
2026-05-12 22:37:16 +0200

    Updated version with navbar and darkmode depending on time

--- script.js
+++ script.js
@@ -1,7 +1,41 @@
-const toggleBtn = document.getElementById("toggleBtn");
+const button = document.getElementById('theme-toggle');
 
-// Listen for click event dark mode
-toggleBtn.addEventListener("click", () => {
-    document.body.classList.toggle("dark");
-    document.body.classList.toggle("light");
-});
+        function getThemeByTime() {
+        const hour = new Date().getHours();
+        console.log(hour);
+
+        // Dark mode from 7 PM to 7 AM
+        if (hour >= 19 || hour < 7) {
+            return 'dark'
+        } else return 'light';
+        }
+
+    
+        function applyTheme(theme) {
+            document.documentElement.setAttribute('data-theme', theme);
+        }
+    
+        function updateButtonText(theme) {
+            if (theme === 'dark') {
+                button.textContent = "Light Mode";
+            } else {
+                button.textContent = "Dark Mode";
+            }
+        }
+    
+        // Load saved theme
+        const savedTheme = localStorage.getItem('theme') || 'light';
+    
+        applyTheme(getThemeByTime());
+        updateButtonText(savedTheme);
+    
+        button.addEventListener('click', () => {
+            const current =
+                document.documentElement.getAttribute('data-theme');
+    
+            const next = current === 'dark' ? 'light' : 'dark';
+    
+            applyTheme(next);
+            localStorage.setItem('theme', next);
+            updateButtonText(next);
+        });
